621e901b3b2bd9836a2abfb0ffbc5b139a13bda0
dirk <ursobeautiful@qq.com>
2018-06-27 14:20:35 +0800

    对以太坊token的爬虫

--- samples/web/spider/HelloSpider2.py
+++ samples/web/spider/HelloSpider2.py
@@ -1,6 +1,5 @@
 from urllib import request
 import ssl
-# import re
 from bs4 import BeautifulSoup
 
 # 去掉ssl的验证
@@ -19,14 +18,10 @@ def page(p):
 
         bs_obj = BeautifulSoup(data, 'html.parser')
 
-        # find_all 寻找所有的a标签，而且href属性是/token开头
-        # token_list = bs_obj.find_all('a', href=re.compile("^/token/"))
-
         # select 类别下的标签
         token_list = bs_obj.select('.hidden-xs h5 a')
         print("total token count = ", len(token_list))
         for i in range(len(token_list)):
-            # print(i, token_list[i])
             print(((p - 1) * 50) + i + 1, token_list[i].string)
 
 
@@ -34,7 +29,7 @@ def page(p):
 def crawl():
 
     # 假设1000页
-    for i in range(100, 1000):
+    for i in range(1, 12):
         print("抓取的页数 =", i)
         page(i)
 
